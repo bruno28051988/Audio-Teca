0cac2bbe59f4208b0730ae91c8f9aa795a10aceb
Bruno Rodrigues Bordin <bbordin@server01.foursys.local>
2021-06-01 20:45:37 -0300

    subindo arquivo

---
+++ app/src/main/java/com/br/audioteca2/ui/home/HomeFragment.java
@@ -0,0 +1,43 @@
+package com.br.audioteca2.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.br.audioteca2.R;
+import com.br.audioteca2.ui.top.TopFragment;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+
+        });
+        return root;
+
+        }
+
+
+    }
+
+
---
+++ app/src/main/java/com/br/audioteca2/ui/home/HomeViewModel.java
@@ -0,0 +1,23 @@
+package com.br.audioteca2.ui.home;
+
+import android.view.View;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+
+}
